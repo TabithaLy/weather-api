5ad65c84b59b33eac54dccda72fa2c4fe1861ad7
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-01 22:28:50 -0400

    APIKey and starting javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var searchFormEl = document.querySelector('#search-form');
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var APIKey = 'e0e4e065129c66722af1075860939b8a'
