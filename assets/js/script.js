63460c820a762eb33aa9246e22d708b7155db2cb
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-03 15:04:08 -0400

    get data from user city input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,20 @@ var resultContentEl = document.querySelector('#result-content');
 var APIKey = 'e0e4e065129c66722af1075860939b8a'
 
 
-var city = document.querySelector('#search-input');
+var city = document.getElementById('searchInput');
 
 // fetch(queryURL);
 
 
-var searchBtn = document.querySelector('.btn');
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var searchBtn = document.getElementById('searchBtn');
 // idea from https://tutorial.tips/how-to-use-fetch-api-in-javascript-to-get-data/
+// console.log(queryURL);
+console.log(city);
 var getData = function (event) {
     event.preventDefault();
+    console.log(city.value);
+    city = city.value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     fetch(queryURL)
     .then(res => res.json())
     .then(data => console.log(data))
